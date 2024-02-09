c7c4ae5c3d98a0870a9f47556feb4d0a21ea340c
Stanislav Nikolov <s.nikolov1000@gmail.com>
2024-02-09 16:40:13 +0000

    CH9 tests done

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -23,6 +23,8 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
+LOGIN_URL = 'rango:login'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
